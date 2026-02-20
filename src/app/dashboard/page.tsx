bdb7351801c226570bde02da4a0f3e601b2f97c8
Juliana <goia.julianakg@gmail.com>
2026-02-19 22:36:49 -0300

    feat: add clinic form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
+//por se tratar de um server component posso acessar diretamente meu banco de dados
+
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
@@ -12,6 +17,14 @@ const DashboardPage = async () => {
 
   if (!session?.user) {
     redirect("/authentication");
+  }
+
+  //user.id da minha tabela seja igual ao user.id do meu usuario logado
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
   }
 
   return (
